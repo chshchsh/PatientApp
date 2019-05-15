57920c4268c239a2a4ade540dccb923fd5f1199b
yair2 <ybendavi@g.jct.ac.il>
2019-05-15 23:26:17 +0300

    finished

--- app/src/main/java/com/jct/bd/patientapp/controller/OpeningActivity.java
+++ app/src/main/java/com/jct/bd/patientapp/controller/OpeningActivity.java
@@ -4,37 +4,20 @@ import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
 
-import com.jct.bd.patientapp.R;
+
+
+import static android.os.SystemClock.sleep;
 
 public class OpeningActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
         setContentView(R.layout.activity_opening);
 
-        ImageView imageView = (ImageView)findViewById(R.id.animImage);
-        imageView.setBackgroundResource(R.drawable.opening_animation);
-        openingAnimation = (AnimationDrawable) imageView.getBackground();
-        openingAnimation.start();
 
-        Thread myThread = new Thread()
-        {
-            @Override
-            public void run()
-            {
-                try {
-                    sleep(4500);
-                    Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
-                    startActivity(intent);
-                    finish();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-        myThread.start();
+        sleep(3000);
+        Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+        startActivity(intent);
     }
 }
