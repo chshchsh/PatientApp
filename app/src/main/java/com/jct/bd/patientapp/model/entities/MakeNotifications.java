8e05b766fcd71290d82641323fe58e457b36188b
yair2 <ybendavi@g.jct.ac.il>
2019-05-16 17:20:40 +0300

    updated

---
+++ app/src/main/java/com/jct/bd/patientapp/model/entities/MakeNotifications.java
@@ -0,0 +1,37 @@
+package com.jct.bd.patientapp.model.entities;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+
+import com.jct.bd.patientapp.model.backend.AlertReceiver;
+
+import java.util.Calendar;
+
+
+public class MakeNotifications {
+    public static void makeNotification(Context context, int hour, int minute, int id) {
+        Calendar time = Calendar.getInstance();
+        time.set(Calendar.HOUR_OF_DAY, hour);
+        time.set(Calendar.MINUTE, minute);
+        time.set(Calendar.SECOND, 0);
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(context, AlertReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, id, intent, 0);
+        if (time.getTimeInMillis() < Calendar.getInstance().getTimeInMillis()) {
+            time.add(Calendar.DATE, 1);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, time.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
+        }
+    }
+    private static void CancelNotification(Context context, int id) {
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(context, AlertReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, id, intent, 0);
+        alarmManager.cancel(pendingIntent);
+    }
+}
